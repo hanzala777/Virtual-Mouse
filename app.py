5039abf56b25ad40932437c173467c2d8d737e7a
Hanzala Ahmed <hanzhanzala@gmail.com>
2023-12-05 20:24:34 +0530

    virtual mouse updates

--- app.py
+++ app.py
@@ -1,27 +1,5 @@
-# from flask import Flask, render_template, request
-# import subprocess
-#
-# app = Flask(__name__, static_url_path='/static')
-#
-#
-# @app.route('/')
-# def index():
-#     return render_template('home.html')
-#
-#
-# @app.route('/run_python', methods=['POST'])
-# def run_python():
-#     # Execute your Python script (hello.py) using subprocess
-#     subprocess.run(['python', 'final.py'])
-#     return render_template('home.html')
-#
-#
-# if __name__ == '__main__':
-#     app.run(debug=False, host='0.0.0.0')
-
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import subprocess
-import os
 
 app = Flask(__name__)
 
@@ -53,4 +31,4 @@ def stop_execution():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
